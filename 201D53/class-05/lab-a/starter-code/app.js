6b8dbc20c28cbb60534a54d8546442c268b857d3
wyatt <grodzilla93@gmail.com>
2019-10-04 23:10:53 -0700

    moving along smoothly, still dont know why three is broken and its driving me crazy.

--- 201D53/class-05/lab-a/starter-code/app.js
+++ 201D53/class-05/lab-a/starter-code/app.js
@@ -83,12 +83,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+var sum4 = sum(testArray[0],testArray[1]);
+var total4 = sum(sum4[0],testArray[2]);
+var string5 = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${total4[0]} is their sum.`;
+return [total4[0],string5];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
