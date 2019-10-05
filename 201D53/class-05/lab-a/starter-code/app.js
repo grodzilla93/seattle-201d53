05887cf37650ee9af0edc0e19468216bad35cbed
wyatt <grodzilla93@gmail.com>
2019-10-04 22:57:03 -0700

    Problem 3 is broken, it looks correct... i have no idea why its broken, but i am pressed for time and moving forward. maybe someone will tell me whats wrong monday

--- 201D53/class-05/lab-a/starter-code/app.js
+++ 201D53/class-05/lab-a/starter-code/app.js
@@ -53,11 +53,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+var addsum = sum(a,b);
+var total3 = sum(addsum[0],c);
+// console.log(total3);
+var string3 = `${a} and ${b} and ${c} sum to ${total3[0]}.`;
+var callmulti = multiply(a,b);
+var multotal = multiply(callmulti[0],c);
+// console.log(multotal);
+var string4 = `The product of ${a} and ${b} and ${c} is ${multotal}.`;
+return [total3[0], multotal[1], string3, string4];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
